61605579bf21abf6365196c157b39a8dc4c22204
aarati01 <131001442+aarati01@users.noreply.github.com>
2025-02-16 16:55:38 -0500

    fontAwesome is added with some regex

--- src/Register.tsx
+++ src/Register.tsx
@@ -1,4 +1,12 @@
-import React from "react";
+import { useRef, useState, useEffect } from "react";
+import {
+  faCheck,
+  faTimes,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
+const usernameRegex = /^(?=.{4,16}$)[a-zA-Z0-9_-]+$/;
+const passwordRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#^])[A-Za-z\d@$!%*?&#^]{8,}$/;
 
 const Register = () => {
   return <div></div>;
